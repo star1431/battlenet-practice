be06282530d4d501fe80128fb29dd0a7ace4084a
JBC <38375312+star1431@users.noreply.github.com>
2022-04-12 17:48:47 +0900

    Add files via upload
    up버튼 추가 / 메뉴바 코드 수정

--- js/jbc_custom.js
+++ js/jbc_custom.js
@@ -1,14 +1,16 @@
 $(function(){
-    swal("배틀넷 연습코딩 사이트입니다.(PC용)", "실제사이트와 연관이 없음을 알립니다.", "warning");
+    swal("배틀넷 연습코딩 사이트입니다.(PC용)", "이 사이트는 상업목적이 아니며 실제사이트와 연관이 없음을 알립니다.", "warning");
     //메뉴바 fixed
     var menuBar= $(".menu-container").offset();
     function headerFixed(){
         if ($(document).scrollTop() > menuBar.top ) {
             $('.header').addClass('fixed-active');
-            $(".menu-container").addClass("scroll-fixed");
+            $(".menu-bar").addClass("scroll-fixed");
+            $('.scroll-up').fadeIn();
         }else {
             $('.header').removeClass('fixed-active');
-            $(".menu-container").removeClass("scroll-fixed");
+            $(".menu-bar").removeClass("scroll-fixed");
+            $('.scroll-up').hide();
         }
     }
     //owl plugin
@@ -36,7 +38,7 @@ $(function(){
         $(this).siblings().show();
     });
     //user modal
-    //각 토글 지정함수
+    //각 토글이벤트 지정함수
      $.userToggle = function(){
         $('.nav-item-modal').toggleClass('_none');
         $('.modal-background').toggleClass('_none');
@@ -50,33 +52,36 @@ $(function(){
         $('.coin-menu .menu-list').toggleClass('_none');
         $('#coin-select').toggleClass('active');
      };
-    //클릭시 드롭
+
+    //클릭시 드롭 (상단 내계정)
     $('.nav-item .user').on('click',function(){
         $.userToggle()
     });
     $('.modal-background').on('click',function(){
         $.userToggle()
     });
+    //메뉴바 게임,코인메뉴
+    $('body').mouseup(function(e){ 
+        if($('.game-menu').has(e.target).length == 0){
+            $('.game-menu .menu-list').addClass('_none');
+            $('#game-select').removeClass('active');
+        } 
+        if($('.coin-menu').has(e.target).length == 0){
+            $('.coin-menu .menu-list').addClass('_none');
+            $('#coin-select').removeClass('active');
+        }
+    });
     $('#game-select').on('click',function(){
         $.menuToggle()
-        $('.coin-menu .menu-list').addClass('_none');
-        $('#coin-select').removeClass('active');
     });
     $('#coin-select').on('click',function(){
         $.coinToggle()
-        $('.game-menu .menu-list').addClass('_none');
-        $('#game-select').removeClass('active');
     });
-    //드롭메뉴 외 클릭(수정중)
-    // $('body').click(function(e){ 
-    //     if($('.menu-button').has(e.target).length==0){
-    //         $('.drop-menu .menu-list').addClass('_none');
-    //         $('.menu-button').removeClass('active');
-    //         console.log('11')
-    //     };  
-    //     console.log('22')
-    // });
     
+    $('.scroll-up').click(function() {
+        $('html, body').animate({scrollTop:0}, 600);
+    });
+    // 윈도우 로드 스크롤
     $(window).on('load scroll',function(){
         headerFixed();
     });
